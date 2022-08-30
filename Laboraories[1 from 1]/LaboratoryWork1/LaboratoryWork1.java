af1fe27dfd3d590927bcafa63e8c8a0688784868
satsapatovsky <satsapatovsky.student@gmail.com>
2022-08-30 14:04:07 +0300

    momental launch explanation guide inside main files

--- Laboraories[1 from 1]/LaboratoryWork1/LaboratoryWork1.java
+++ Laboraories[1 from 1]/LaboratoryWork1/LaboratoryWork1.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+
+//Just run the psvm inside LaboratoryWork1
 public class LaboratoryWork1 {
         public static void main(String[] args) throws InterruptedException {
             MenuOrganizations menu = new MenuOrganizations();
--- Practices[2 from 2]/PracticeWork2/Practice2.java
+++ Practices[2 from 2]/PracticeWork2/Practice2.java
@@ -1,3 +1,5 @@
+//Just run the psvm inside Practice2
+
 public class Practice2 {
     public static void main(String[] args) {
      Menu mainMenu = new Menu();
